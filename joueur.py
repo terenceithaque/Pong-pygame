f2765ffb9e2751a0662eb4e7fff89320180b0391
terenceithaque <terence.lethierry@protonmail.com>
2023-07-04 10:21:48 +0200

    Affichage de la raquette du joueur

--- joueur.py
+++ joueur.py
@@ -4,3 +4,17 @@ import pygame  # Importer Pygame
 
 class Joueur(pygame.sprite.Sprite):
     "Raquette contrôlée par le joueur"
+
+    def __init__(self, x, y):
+        super().__init__()
+        # Image qui représente le joueur
+        self.image = pygame.Surface((10, 60))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.couleur = (255, 255, 255)  # Couleur d'affichage du joueur
+
+    def update(self):
+        "Mettre à jour la position de la raquette du joueur"
+        pass
--- main.py
+++ main.py
@@ -7,9 +7,20 @@ pygame.display.init()
 window = pygame.display.set_mode((800, 600))  # Créer une fenêtre de jeu
 pygame.display.set_caption("Pong !")
 
+x = 50
+y = 40
+joueur = Joueur(x, y)
+
 is_running = True  # Est-ce que le jeu est en cours d'exécution ?
 
 while is_running:  # Tant que le jeu est exécuté
     for evenement in pygame.event.get():  # Pour chaque évènement intercepté
         if evenement == pygame.QUIT:  # Si cet évènement est "quitter Pygame"
             is_running = False  # Le jeu n'est plus exécuté
+
+    window.fill((0, 0, 0))
+
+    # Dessiner le joueur à l'écran
+    pygame.draw.rect(window, joueur.couleur, joueur)
+
+    pygame.display.flip()
